e92b6c9bd9371dfbead108e0871a3d4d5cbd2ad0
Benoît Ngô <ngobenoit@gmail.com>
2017-09-01 11:18:49 +0200

    Removing unused method

--- src/GraphqlTypeInterface.php
+++ src/GraphqlTypeInterface.php
@@ -5,5 +5,4 @@ use GraphQL\Type\Definition\ObjectType;
 
 interface GraphqlTypeInterface { 
     static public function getType($typeRegistry): ObjectType;
-    public function graphqlSerialize(): array;
 }
