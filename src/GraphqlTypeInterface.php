d57cb1bfbdd5b5f43cd2de35e68c757e5e0f2a6a
Benoît Ngô <ngobenoit@gmail.com>
2017-09-13 15:34:16 +0200

    Fixing type hint

--- src/GraphqlTypeInterface.php
+++ src/GraphqlTypeInterface.php
@@ -1,8 +1,8 @@
 <?php
 namespace PsCs\Harmony\Graphql\Tool;
-use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
 
 
 interface GraphqlTypeInterface { 
-    static public function getType($typeRegistry): ObjectType;
+    static public function getType($typeRegistry): Type;
 }
